9b4df004b2f21754ab93ff9a4aea17b2512f31a1
bomen-bomen <111312997+bomen-bomen@users.noreply.github.com>
2024-03-03 06:21:06 -0700

    Update test_app.py
    change app to APP

--- test_app.py
+++ test_app.py
@@ -5,14 +5,14 @@ import tempfile
 
 import pytest
 
-from app import app
+from app import APP
 
 @pytest.fixture
 def client():
-  app.config['TESTING'] = True
-  client = app.test_client()
+  APP.config['TESTING'] = True
+  client = APP.test_client()
 
-yield client
+  yield client
 
 def test_valid_transaction(client):
   card = {
